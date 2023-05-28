49f3330170a146a850d63ec1f29438a190e6465e
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-05-28 19:15:37 -0400

    Complete Lab

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { minutesToRead } from "../utility";
 
 function Article({ date = "January 1, 1970", preview, title, minutes }) {
   return (
     <article>
       <h3>{title}</h3>
       <small>{date}</small>
+      {minutesToRead(minutes)}
       <p>{preview}</p>
     </article>
   );
